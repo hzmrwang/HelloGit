a1767877fa79761b009db3be1803a07b9dc4c04b
hzmrwang <1083596145@qq.com>
2021-02-25 21:53:23 +0800

    Update HelloGit.java

--- HelloGit/src/main/java/cn/vipfeng/HelloGit.java
+++ HelloGit/src/main/java/cn/vipfeng/HelloGit.java
@@ -3,9 +3,10 @@ package cn.vipfeng;
 public class HelloGit {
     public static void main(String[] args) {
         System.out.println("Hello World");
-        System.out.println("gengxin");
+        System.out.println("123456");
         System.out.println("创建分支");
         System.out.println("主干添加");
         System.out.println("分支添加");
+        System.out.println("在线更新");
     }
 }
