d3c4f4754b71f6f20569953e0f1dd2d81a457182
hzmrwang <1083596145@qq.com>
2021-02-25 21:35:34 +0800

    add file

--- HelloGit/src/main/java/cn/vipfeng/HelloGit.java
+++ HelloGit/src/main/java/cn/vipfeng/HelloGit.java
@@ -3,5 +3,6 @@ package cn.vipfeng;
 public class HelloGit {
     public static void main(String[] args) {
         System.out.println("Hello World");
+        System.out.println("gengxin");
     }
 }
